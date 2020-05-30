c611edd5c7854d2667c728d83dd95be28e2bdb5d
Vincent10200 <66163450+Vincent10200@users.noreply.github.com>
2020-05-30 12:46:22 +0200

    Add files via upload

--- Javascript-test/index.js
+++ Javascript-test/index.js
@@ -6,7 +6,7 @@ function button()
 
     if (Hackbutton > 1) {
         alert('You hacked the website.')
-        location.href = 'https://rocketsimulator.ga';
+        
       } else {
         alert('Your not allowed to visit this website.')
       }
